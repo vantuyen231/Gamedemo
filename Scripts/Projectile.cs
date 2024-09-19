848f6fc601cd370b1b03d58a6fb04aafa3e5a47f
vantuyen231 <dovantuyen21112002@gmail.com>
2024-09-19 22:36:47 +0700

    test

---
+++ Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Weapon weaponScript;
+    public float speed;
+    public float lifeTime;
+    public int damage;
+
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector2.left * speed* Time.deltaTime);
+    }
+
+    void DestroyProjectile() 
+    { 
+        Destroy(gameObject); 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+            collision.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectile(); 
+    }
+}
